beb00a3fe8fc510ad2ca5934811226d5b5102634
Anu Thomas <anumolthomas009@gmail.com>
2017-08-05 12:10:18 +0530

    Login functionality added

--- app/login/loginController.js
+++ app/login/loginController.js
@@ -1,4 +1,28 @@
 
-app.controller("loginController", function ($scope) {
+app.controller("loginController", function ($scope , $location) {
     console.log("Login page is opened.... ")
+    $scope.name = "admin";
+	$scope.password = "admin";
+    $scope.validation = validation;
+	function validation() {
+		var name = $scope.name;
+    	var password = $scope.password;
+
+    if (name == "admin"&& password == "admin") 
+	{
+		console.log("ok");
+		$location.path("/dashboard");
+    }else 
+	if(name=="")
+	{
+    alert("Enter name");
+    return false;  
+	}else
+	if(password =="")
+	{
+    alert("Enter Password");
+    return false;
+  	}
+	}
+
 });
